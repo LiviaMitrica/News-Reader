f9014bb8e8562718f60815d4dd9db5792a68c767
Livia Mitrica <livia.maria.mitrica@gmail.com>
2020-07-02 13:05:23 +0300

    .gitignore 6

--- app/src/main/java/com/liviamitrica/nr/ui/main/listener/NewsArticleHandler.java
+++
@@ -1,7 +0,0 @@
-package com.liviamitrica.nr.ui.main.listener;
-
-import com.liviamitrica.nr.ui.main.model.ArticleItemViewModel;
-
-public interface NewsArticleHandler {
-    void onItemSelected(ArticleItemViewModel item);
-}

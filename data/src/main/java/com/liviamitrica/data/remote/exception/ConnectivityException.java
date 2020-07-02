08350947194bc82524624f47e740bbea13108111
Livia Mitrica <livia.maria.mitrica@gmail.com>
2020-07-02 11:33:01 +0300

    add and ignore files 2

--- data/src/main/java/com/liviamitrica/data/remote/exception/ConnectivityException.java
+++
@@ -1,8 +0,0 @@
-package com.liviamitrica.data.remote.exception;
-
-public class ConnectivityException extends RuntimeException{
-
-    public ConnectivityException(String message) {
-        super("Server was not reached: " + message);
-    }
-}
